8371810d8e88858939e6bde02fc4b799f96e1a04
Justinefoulq <justinefoulquier@hotmail.fr>
2019-05-09 14:53:45 +0200

    architechture bis

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,8 @@
 const express = require('express');
-const router = express.Router()
+const router = express.Router();
 
-
-router.get('/' ,(req,res) => {
-	res.send('Hello World');
+router.get('/', (req,res)=>{
+	res.send('Hello world');
 })
 
 module.exports = router;
