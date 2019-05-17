e2fc18a757d112b1af2fdb2706a8126b438cabaf
Justinefoulq <justinefoulquier@hotmail.fr>
2019-05-17 12:05:48 +0200

    affichage avis

--- src/routes/location.js
+++ src/routes/location.js
@@ -17,8 +17,9 @@ router.get('/location/:NumLoc', async (req,res) => {
 	const photos = await pool.query('SELECT CheminPhoto FROM photo WHERE NumLoc=?',[NumLoc]);
 	const semdispo = await pool.query('SELECT * FROM semaine S WHERE S.NumSemaine NOT IN (SELECT E.NumSemaine FROM effectue E, reservation R WHERE R.NumResa=E.NumResa AND R.ResaRefus!=1 AND E.NumLoc=?)',[NumLoc]);
 	const saison =await pool.query('SELECT S.NumSaison , S.LibSaison , S.SemDebutSaison, S.SemFinSaison ,T.PrixSemaine, T.SuplementNbPersSup4 FROM saison S, tarif T,applique A WHERE A.NumSaison=S.NumSaison AND T.NumTarif=A.NumTarif AND A.NumLoc=? ORDER BY S.SemDebutSaison',[NumLoc]);
-   
-    res.render('location' ,{ location : location[0], photos,semdispo,saison});
+   	const avis = await pool.query ('SELECT A.TitreAvis, A.NoteAvis,A.CommentaireAvis, A.DateAvis, C.NomClient, C.PrenomClient FROM avis A, client C WHERE A.NumLoc=? AND C.NumClient=A.NumClient',[NumLoc])
+   	console.log(avis)
+    res.render('location' ,{ location : location[0], photos,semdispo,saison,avis});
 });
 
 
